de55838ab0b807657f9b370117dce0c71a9e8732
Gabriel Bernardi <115042820+GoldenLeap@users.noreply.github.com>
2025-08-22 16:47:32 -0300

    22.08.2025 Exercicio sobre construtores

--- Aula 06/Exercicio php.php
+++ Aula 06/Exercicio php.php
@@ -17,11 +17,13 @@ $objeto1->categoria = "Smartphone";
 $objeto1->qtd = 25;
 $objeto1->valor = 3800.00;
 
+$objeto2 = new Produtos();
 $objeto2->nome = "Dell Inspiron 15";
 $objeto2->categoria = "Notebook";
 $objeto2->qtd = 15;
 $objeto2->valor = 5009.00;
 
+$objeto3 = new Produtos();
 $objeto3->nome = "Philco Wave PFO01BTP - 56605001";
 $objeto3->categoria = "Fones de ouvido Bluetooth";
 $objeto3->qtd = 30;
