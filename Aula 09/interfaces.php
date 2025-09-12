2c1260deab20c50aca4e6cc2d39f4e08c28fd3ec
Gabriel Bernardi <115042820+GoldenLeap@users.noreply.github.com>
2025-09-12 13:49:09 -0300

    Faz as classes pentagono e hexagono que implementam a interface Forma

--- Aula 09/interfaces.php
+++ Aula 09/interfaces.php
@@ -15,15 +15,18 @@
 
     // Caso tnham mais arquivos que formam o backend de uma página web e possuem a mesma raiz, o namespace será o mesmo
 
-    namespace Aula_09;
-    
-    interface Pagamento{
+namespace Aula_09;
+
+    interface Pagamento
+    {
         public function pagar($valor);
     }
 
 
-    class CartaoDeCredito implements Pagamento{
-        public function pagar($valor){
+    class CartaoDeCredito implements Pagamento
+    {
+        public function pagar($valor)
+        {
             echo "Pagamento realizado com cartão de crédito, valor: $valor\n";
         }
     }
@@ -36,25 +39,55 @@
 
     // Circulo deve receber o raio e calcular a área
 
-    interface Forma {
+    interface Forma
+    {
         public function calcularArea($x);
     }
 
-    class Circulo implements Forma{
-        public function calcularArea($raio){
+    class Circulo implements Forma
+    {
+        public function calcularArea($raio)
+        {
             $raio = (float)$raio;
             $area = pow($raio, 2) * M_PI;
             return round($area, 2);
         }
     }
-    class Quadrado implements Forma{
-        public function calcularArea($lado){
-            return pow($lado,2);
+    class Quadrado implements Forma
+    {
+        public function calcularArea($lado)
+        {
+            return pow($lado, 2);
+        }
+    }
+
+    // Crie a classe pentagono, e calcule a área do mesmo
+
+    class Pentagono implements Forma
+    {
+        public function calcularArea($lado)
+        {
+            $area = (1 / 4) * sqrt(5 * (5 + 2 * sqrt(5))) * pow($lado, 2);
+            return number_format($area, 2);
+        }
+    }
+
+    // Crie a classe hexagono, e calcule a área do mesmo.
+    class Hexagono implements Forma
+    {
+        public function calcularArea($lado)
+        {
+            $area = (3 * sqrt(3) * pow($lado, 2)) / 2;
+            return number_format($area, 2);
         }
     }
 
     $quadrado = new Quadrado();
     $circulo = new Circulo();
+    $pentagono = new Pentagono();
+    $hexagono = new Hexagono();
 
-    echo "Área do quadrado: ". $quadrado->calcularArea((float)readline("Lado do quadrado: "))."cm²\n";
-    echo "Área do circulo: " . $circulo->calcularArea((float)readline("Digite o raio do circulo: ")) . "cm²\n";
+    echo "Área do Quadrado: " . $quadrado->calcularArea((float)readline("Lado do quadrado: ")) . "cm²\n";
+    echo "Área do Circulo: " . $circulo->calcularArea((float)readline("Digite o raio do circulo: ")) . "cm²\n";
+    echo "Área do Pentaogno: " . $pentagono->calcularArea((float)readline("Lado do pentagono: ")) . "cm²\n";
+    echo "Área do Hexagono: " . $hexagono->calcularArea((float)readline("Lado do hexagono: ")) . "cm²\n";
